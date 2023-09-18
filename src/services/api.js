97364dc9967750d1c8faee0f2be0f3e0f9afd128
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-09-18 20:51:05 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://api-e-commerce-updater-pro.onrender.com',
-    timeout: 1000,
+    timeout: 10000,
     headers: { 'Content-type': 'application/json' }
 });
